454077e5395a7c16011e898e766f0a0a3fc27173
Alpha Ouédraogo <c.alpha.ouedraogo@gmail.com>
2016-01-28 16:33:27 +0100

    Revert "Commit 1.0"
    This reverts commit 25fac111682a7b7e9615ed41ad8d8a463f3a8db0.

---
+++ client/templates/hello.js
@@ -0,0 +1,11 @@
+/*
+Template.hello.helpers({
+  name: function () {
+    if (Meteor.user()) {
+      return Users.getDisplayName(Meteor.user());
+    } else {
+      return "You";
+    }
+  }
+});
+*/
